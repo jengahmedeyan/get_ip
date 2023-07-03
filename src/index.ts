7d8f3fb80f12cd50eac031a712353ca206a17ddd
ahmedeyanjeng <aejeng@insistglobal.com>
2023-07-03 12:04:01 +0000

    modularise the code to separate concern

--- src/index.ts
+++ src/index.ts
@@ -1,72 +1,76 @@
+
 import os from 'os';
 import dotenv from 'dotenv';
 import fs from 'fs';
-import {Config} from "./types";
+import { Config } from './types';
 
-function fetchIPAddress(config: Config): void {
+async function getIPAddress(): Promise<string> {
 	const interfaces = os.networkInterfaces();
+
 	for (const interfaceName in interfaces) {
 		const networkInterface = interfaces[interfaceName];
+
 		for (const network of networkInterface as os.NetworkInterfaceInfo[]) {
 			if (network.family === 'IPv4' && !network.internal) {
-				if (!config.envFilePath) {
-					config.envFilePath = '.env';
-				}
-
-				if (!config.envVariableName) {
-					config.envVariableName = 'IP_ADDRESS';
-				}
-
-				const envData = fs.existsSync(config.envFilePath)
-					? dotenv.parse(fs.readFileSync(config.envFilePath))
-					: {};
-
-				if (envData[config.envVariableName] !== network.address) {
-					envData[config.envVariableName] = network.address;
-
-					let newEnvContent = '';
-					Object.keys(envData).forEach((key) => {
-						newEnvContent += `${key}=${envData[key]}\n`;
-					});
-
-					fs.writeFileSync(config.envFilePath, newEnvContent);
-				}
-
-				return;
+				return network.address;
 			}
 		}
 	}
 
-	console.error('Unable to fetch IP address');
+	throw new Error('Unable to fetch IP address');
 }
 
-const userConfigFilePath = 'get_ip_config.json';
+async function updateEnvironmentVariables(ipAddress: string, config: Config): Promise<void> {
+	if (!config.envFilePath) {
+		config.envFilePath = '.env';
+	}
 
+	if (!config.envVariableName) {
+		config.envVariableName = 'IP_ADDRESS';
+	}
+
+	const envData = fs.existsSync(config.envFilePath)
+		? dotenv.parse(fs.readFileSync(config.envFilePath))
+		: {};
+
+	if (envData[config.envVariableName] !== ipAddress) {
+		envData[config.envVariableName] = ipAddress;
+
+		let newEnvContent = '';
+		Object.keys(envData).forEach((key) => {
+			newEnvContent += `${key}=${envData[key]}\n`;
+		});
+
+		fs.writeFileSync(config.envFilePath, newEnvContent);
+	}
+}
+
+const userConfigFilePath = 'ip_config.json';
 const defaultConfig: Config = {
 	envFilePath: undefined,
 	envVariableName: undefined,
 };
-
 let userConfig: Config = defaultConfig;
+
 if (fs.existsSync(userConfigFilePath)) {
-	const fileData = fs.readFileSync(userConfigFilePath, 'utf8');
 	try {
+		const fileData = fs.readFileSync(userConfigFilePath, 'utf8');
 		const parsedConfig = JSON.parse(fileData);
 		userConfig = { ...defaultConfig, ...parsedConfig };
 	} catch (error) {
-		console.error('Invalid user config file:', error);
+		console.error('Error reading or parsing user config file:', error);
 	}
 }
 
-fetchIPAddress(userConfig);
-
-export function getIp(): string | undefined {
-	if (!userConfig.envFilePath || !userConfig.envVariableName) {
-		console.error('Invalid user config');
-		return undefined;
+export async function getIp(): Promise<string> {
+	try {
+		const ipAddress = await getIPAddress();
+		await updateEnvironmentVariables(ipAddress, userConfig);
+		process.env.IP_ADDRESS = ipAddress;
+		dotenv.config();
+		return ipAddress;
+	} catch (error) {
+		console.error('Error fetching or updating IP address:', error);
+		throw error;
 	}
-
-	const envData = dotenv.parse(fs.readFileSync(userConfig.envFilePath));
-	return envData[userConfig.envVariableName];
 }
-
